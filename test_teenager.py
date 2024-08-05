95381839b12825735ce6d3cace15ce3a53ce180a
VeraSolarova <165845970+VeraSolarova@users.noreply.github.com>
2024-08-05 16:45:45 +0200

    Add files via upload

---
+++ test_teenager.py
@@ -0,0 +1,7 @@
+#  teenager je od 13-19 let, jinak jen odpoved nejsi teenager
+vek = input('Kolik je Ti let? ')
+vek = int(vek)
+if vek >= 13 and vek <= 19:
+    print("Jsi teenager")
+else:
+    print("nejsi teenager")
